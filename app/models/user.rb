8cb882b411dcd28687294cd1e35a0aa3b63a068b
brynknowles <bryn.knowles@gmail.com>
2020-10-09 13:38:29 -0400

    twitter follows dq work in progress

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+    has_many :followeds, through: :active_relationships, source: :followed
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
+
+    def follow(user)
+        followers = self.followeds
+        followers << user
+    end
+
+end
